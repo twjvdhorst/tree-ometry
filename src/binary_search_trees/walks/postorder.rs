f194c71d5ed4da43840278d43bbc71195ebf9f5f
twjvdhorst <thijs.vd.horst@outlook.com>
2026-01-03 14:07:52 +0100

    Refactor PostorderWalk into a single struct

--- src/binary_search_trees/walks/postorder.rs
+++ src/binary_search_trees/walks/postorder.rs
@@ -1,4 +1,4 @@
-use std::{marker::PhantomData, ops::DerefMut};
+use std::{fmt::Debug, marker::PhantomData, ops::DerefMut};
 
 use lending_iterator::prelude::*;
 
@@ -32,43 +32,67 @@ impl<E> DFSStack<E> {
             self.pop().map(|(last, _)| last)
         } else { None }
     }
+
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
 }
 
 pub struct PostorderWalk<'lt, N, E, F> {
-    completed: bool,
-    root: &'lt mut N,
-    left_walk: Option<PostorderEdgeWalk<'lt, N, E, F>>,
-    right_walk: Option<PostorderEdgeWalk<'lt, N, E, F>>,
+    root_state: Option<(&'lt mut N, Vec<Side>)>,
+    frontier_stack: Vec<E>, // Edges that we have encountered, but not traversed yet.
+    visited_stack: DFSStack<E>, // Edges that we have encountered and traversed, but not reported yet.
+    instruction_fn: F,
 }
 
-impl<'lt, N, E, F> PostorderWalk<'lt, N, E, F> 
+impl<'lt, N, E, F> PostorderWalk<'lt, N, E, F>
 where 
     N: BinarySearchTreeNode<Wrapper = N, Edge = E>,
     E: DerefMut<Target = N>,
-    F: Fn(&N) -> WalkInstruction + Clone,
+    F: Fn(&N) -> WalkInstruction,
 {
-    pub fn new(root: &'lt mut N, instruction_fn: F) -> Self {
-        let (left_walk, right_walk) = match (instruction_fn)(root) {
-            WalkInstruction::Left => (
-                root.detach_left().map(|left| PostorderEdgeWalk::new(left, instruction_fn)),
-                None,
-            ),
-            WalkInstruction::Right => (
-                None,
-                root.detach_right().map(|right| PostorderEdgeWalk::new(right, instruction_fn)),
-            ),
-            WalkInstruction::Both => (
-                root.detach_left().map(|left| PostorderEdgeWalk::new(left, instruction_fn.clone())),
-                root.detach_right().map(|right| PostorderEdgeWalk::new(right, instruction_fn)),
-            ),
-            WalkInstruction::None => (None, None),
+    pub fn new(root_edge: &'lt mut N, instruction_fn: F) -> Self {
+        let mut root_state = None;
+        let mut frontier_stack = Vec::new();
+        match (instruction_fn)(&root_edge) {
+            WalkInstruction::Left => {
+                if let Some(left) = root_edge.detach_left() {
+                    frontier_stack.push(left);
+                    root_state = Some((root_edge, vec![Side::Left]));
+                }
+            },
+            WalkInstruction::Right => {
+                if let Some(right) = root_edge.detach_right() {
+                    frontier_stack.push(right);
+                    root_state = Some((root_edge, vec![Side::Right]));
+                }
+            },
+            WalkInstruction::Both => {
+                match (root_edge.detach_left(), root_edge.detach_right()) {
+                    (Some(left), Some(right)) => {
+                        frontier_stack.push(right);
+                        frontier_stack.push(left);
+                        root_state = Some((root_edge, vec![Side::Right, Side::Left]));
+                    },
+                    (Some(left), _) => {
+                        frontier_stack.push(left);
+                        root_state = Some((root_edge, vec![Side::Left]));
+                    },
+                    (_, Some(right)) => {
+                        frontier_stack.push(right);
+                        root_state = Some((root_edge, vec![Side::Right]));
+                    },
+                    _ => (),
+                }
+            },
+            WalkInstruction::None => (),
         };
 
         Self {
-            completed: false,
-            root,
-            left_walk,
-            right_walk,
+            root_state,
+            frontier_stack,
+            visited_stack: DFSStack::new(),
+            instruction_fn,
         }
     }
 }
@@ -76,7 +100,7 @@ where
 #[gat]
 impl<'lt, N, E, F> LendingIterator for PostorderWalk<'lt, N, E, F>
 where 
-    N: BinarySearchTreeNode<Wrapper = N, Edge = E>,
+    N: BinarySearchTreeNode<Wrapper = N, Edge = E, Key: Debug>,
     E: DerefMut<Target = N>,
     F: Fn(&N) -> WalkInstruction,
 {
@@ -86,125 +110,80 @@ where
         = &'next mut N;
 
     fn next(self: &'_ mut PostorderWalk<'lt, N, E, F>) -> Option<&'_ mut N> {
-        if let Some(left_walk) = self.left_walk.as_mut() && let Some(next) = left_walk.next() {
-            return Some(next);
+        if self.frontier_stack.is_empty() && self.visited_stack.is_empty() {
+            if let Some((root, _)) = self.root_state.take() {
+                return Some(root);
+            } else {
+                return None;
+            }
         }
-        if let Some(right_walk) = self.right_walk.as_mut() && let Some(next) = right_walk.next() {
-            return Some(next);
-        }
-        if !self.completed {
-            self.completed = true;
-            Some(self.root)
-        } else { None }
-    }
-}
-
-pub struct PostorderEdgeWalk<'lt, N, E, F> {
-    completed: bool,
-    frontier_stack: Vec<E>, // Edges that we have encountered, but not traversed yet.
-    visited_stack: DFSStack<E>, // Edges that we have encountered and traversed, but not reported yet.
-    instruction_fn: F,
-    _node_ref: PhantomData<&'lt N>,
-}
-
-impl<'lt, N, E, F> PostorderEdgeWalk<'lt, N, E, F>
-where 
-    N: BinarySearchTreeNode<Wrapper = N, Edge = E>,
-    E: DerefMut<Target = N>,
-    F: Fn(&N) -> WalkInstruction,
-{
-    pub fn new(root_edge: E, instruction_fn: F) -> Self {
-        Self {
-            completed: false,
-            frontier_stack: vec![root_edge],
-            visited_stack: DFSStack::new(),
-            instruction_fn,
-            _node_ref: PhantomData,
-        }
-    }
-}
-
-#[gat]
-impl<'lt, N, E, F> LendingIterator for PostorderEdgeWalk<'lt, N, E, F>
-where 
-    N: BinarySearchTreeNode<Wrapper = N, Edge = E>,
-    E: DerefMut<Target = N>,
-    F: Fn(&N) -> WalkInstruction,
-{
-    type Item<'next>
-    where 
-        Self: 'next,
-        = &'next mut E;
-
-    fn next(self: &'_ mut PostorderEdgeWalk<'lt, N, E, F>) -> Option<&'_ mut E> {
-        if self.completed { return None; }
-
-        // Report fully expanded edges first
+        // First check for fully expanded nodes, which we report first.
         if let Some(edge) = self.visited_stack.pop_if_expanded() {
             if let Some((mut parent, mut sides)) = self.visited_stack.pop() {
                 let side = sides.pop().unwrap(); // Can unwrap safely: parent is not fully expanded.
                 parent.attach_child(side, edge);
                 self.visited_stack.push(parent, sides);
 
-                return self.visited_stack.last_edge_mut().and_then(move |parent| parent.get_edge_mut(side));
+                return self.visited_stack.last_edge_mut().and_then(move |parent| parent.get_child_node_mut(side));
             } else {
-                // Edge is root. Put back on frontier_stack to be able to return a reference to it that lives long enough.
-                self.frontier_stack.push(edge);
-                self.completed = true;
-                return self.frontier_stack.last_mut();
+                // Edge is incident to the root node.
+                let (root, mut sides) = self.root_state.take().unwrap(); // Can unwrap safely: root edge exists since there is an unreported edge.
+                let side = sides.pop().unwrap(); // Can unwrap safely: root edge has unreported children.
+                root.attach_child(side, edge);
+                self.root_state = Some((root, sides.clone()));
+
+                return self.root_state.as_mut().unwrap().0.get_child_node_mut(side);
             }
         }
+        
+        // Expand edges on the frontier.
+        let current = loop {
+            let mut current = self.frontier_stack.pop()?;
 
-        let next_edge = loop {
-            let mut edge = self.frontier_stack.pop()?;
-
-            match (self.instruction_fn)(&edge) {
+            match (self.instruction_fn)(&current) {
                 WalkInstruction::Left => {
-                    if let Some(left) = edge.detach_left() {
+                    if let Some(left) = current.detach_left() {
                         self.frontier_stack.push(left);
-                        self.visited_stack.push(edge, vec![Side::Left]);
-                    } else { break edge }
+                        self.visited_stack.push(current, vec![Side::Left]);
+                    } else { break current }
                 },
                 WalkInstruction::Right => {
-                    if let Some(right) = edge.detach_right() {
+                    if let Some(right) = current.detach_right() {
                         self.frontier_stack.push(right);
-                        self.visited_stack.push(edge, vec![Side::Right]);
-                    } else { break edge }
+                        self.visited_stack.push(current, vec![Side::Right]);
+                    } else { break current }
                 },
                 WalkInstruction::Both => {
-                    match (edge.detach_left(), edge.detach_right()) {
+                    match (current.detach_left(), current.detach_right()) {
                         (Some(left), Some(right)) => {
                             self.frontier_stack.push(right);
                             self.frontier_stack.push(left);
-                            self.visited_stack.push(edge, vec![Side::Right, Side::Left]);
+                            self.visited_stack.push(current, vec![Side::Right, Side::Left]);
                         },
                         (Some(left), _) => {
                             self.frontier_stack.push(left);
-                            self.visited_stack.push(edge, vec![Side::Left]);
+                            self.visited_stack.push(current, vec![Side::Left]);
                         },
                         (_, Some(right)) => {
                             self.frontier_stack.push(right);
-                            self.visited_stack.push(edge, vec![Side::Right]);
+                            self.visited_stack.push(current, vec![Side::Right]);
                         },
-                        _ => break edge,
+                        _ => break current,
                     };
                 },
-                WalkInstruction::None => break edge,
+                WalkInstruction::None => break current,
             }
         };
 
-        // Re-attach the edge to its incident parent before reporting it.
+        // Re-attach the edge to its incident parent node before reporting it.
         if let Some((mut parent, mut sides)) = self.visited_stack.pop() {
             let side = sides.pop().unwrap(); // Can unwrap safely: parent is not fully expanded.
-            parent.attach_child(side, next_edge);
+            parent.attach_child(side, current);
             self.visited_stack.push(parent, sides);
 
-            self.visited_stack.last_edge_mut().and_then(move |parent| parent.get_edge_mut(side))
+            self.visited_stack.last_edge_mut().and_then(move |parent| parent.get_child_node_mut(side))
         } else {
-            // Edge is root. Put back on frontier_stack to be able to return a reference to it that lives long enough.
-            self.frontier_stack.push(next_edge);
-            self.completed = true;
-            self.frontier_stack.last_mut()
+            None
         }
     }
 }
